60a1b71611ccbbc687b876b0f5981e0f8195df6a
ToddRizley <todd.rizley@gmail.com>
2016-07-07 09:34:41 -0400

    added search_and_change method

--- prybegone/lib1.rb
+++ prybegone/lib1.rb
@@ -2,11 +2,19 @@ class PryBeGone
   def welcome
   puts "Welcome to pry-be-gone! Would you like to remove or comment-out all binding.pry statements? (remove/comment)"
   end
+
   def get_user_input
      @user_input = gets.chomp
   end
 
   def search_and_change(arg1, arg2)
+    Dir.glob('**/*.rb').each do |file|
+      if file != 'lib1.rb'
+        text = File.read(file) 
+        content = text.gsub("#{arg1}", "#{arg2}")
+        File.open(file, "w") { |file| file << content }
+      end
+    end
   end
 
 def run
@@ -14,33 +22,14 @@ def run
   self.welcome
   self.get_user_input
 
-
-
- 
   if @user_input.downcase == "comment-out"
-    Dir.glob('**/*.rb').each do |file|
-      if file != 'lib1.rb'
-        text = File.read(file) 
-        content = text.gsub("binding.pry", "#binding.pry")
-        File.open(file, "w") { |file| file << content }
-      end
-    end
+    self.search_and_change("binding.pry", "#binding.pry")
+   
   elsif @user_input.downcase == "remove"
-      Dir.glob('**/*.rb').each do |file|
-      if file != 'test.rb'
-        text = File.read(file) 
-        content = text.gsub("binding.pry", "")
-        File.open(file, "w") { |file| file << content }
-      end
-    end
+    self.search_and_change("binding.pry", "")
+      
   elsif @user_input.downcase == "comment-in"
-    Dir.glob('**/*.rb').each do |file|
-      if file != 'test.rb'
-        text = File.read(file) 
-        content = text.gsub("#binding.pry", "binding.pry")
-        File.open(file, "w") { |file| file << content }
-      end
-    end
+    self.search_and_change("#binding.pry", "binding.pry")
   end
 end
 end
