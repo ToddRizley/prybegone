1c5ccb86d15c72119429a12358c40ffa1b400c25
ToddRizley <todd.rizley@gmail.com>
2016-10-17 20:20:05 -0400

    fix file iteration method, include Dir.pwd

--- lib/prybegone.rb
+++ lib/prybegone.rb
@@ -1,34 +1,40 @@
-require "prybegone/version"
-
+# require "prybegone/version"
+require 'pry'
   class PryBeGone
 
     @@input_flag = ARGV
 
     def search_and_change(arg1, arg2)
-      Dir.glob('**/**.rb').each do |file|
-        if file != 'prybegone.rb'
-          text = File.read(file) 
+      @curr_dir = Dir.pwd
+      Dir.glob("#@curr_dir/**.rb").each do |file|
+        unless file.include? "prybegone.rb"
+          text = File.read(file)
           content = text.gsub("#{arg1}", "#{arg2}")
           File.open(file, "w") { |file| file << content }
         end
       end
     end
 
+    def self.this_file
+      Dir.pwd
+    end
+
     def run
       if @@input_flag.include?("-co")
-         self.search_and_change(" binding.pry", "#binding.pry")
+        self.search_and_change(" binding.pry", "#binding.pry")
         self.search_and_change("\nbinding.pry", "\n#binding.pry")
-       
+
       elsif @@input_flag.include?("-r")
         self.search_and_change(" binding.pry", "")
         self.search_and_change("\nbinding.pry", "\n")
-          
+
       elsif @@input_flag.include?("-ci")
         self.search_and_change("#binding.pry", "binding.pry")
         self.search_and_change("\n#binding.pry", "\nbinding.pry")
       end
     end
   end
+
 
 pbg = PryBeGone.new
 pbg.run
